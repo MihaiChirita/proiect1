ee627f776fe2f2a62826d96884717531b83534eb
Ubuntu <ubuntu@ip-172-31-38-79.us-east-2.compute.internal>
2021-01-01 10:10:39 +0000

    initial commit

---
+++ se.sh
@@ -0,0 +1,15 @@
+mkdir backend
+cd backend
+touch server.js
+npm init -y
+npm istall --save express
+npm istall --save sequelize
+npm istall --save mysql2
+mkdir config
+mkdir toutes
+mkdir models
+mkdir controllers
+cd..
+npm istall -g create-react-app
+npx create-react-app frontend
+
